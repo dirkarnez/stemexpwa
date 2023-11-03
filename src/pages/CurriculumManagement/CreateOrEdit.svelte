719f701b18bc8d3790cbf2865ae8d44fc769e6c5
dirkarnez <smalldirkalex@gmail.com>
2023-11-03 22:32:19 +0800

    - wip

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -117,6 +117,8 @@
 				setFields(icon_id_key, _data[icon_id_key], true);
 				setFields(parent_id_key, _data[parent_id_key], true);
 				setFields(blog_entries_field_key, (_data[blog_entries_field_key] || []), true);
+				setFields(youtube_video_entries_field_key, (_data[youtube_video_entries_field_key] || []), true);
+				setFields(information_entries_field_key, (_data[information_entries_field_key] || []), true);
 			})
 		}
 
