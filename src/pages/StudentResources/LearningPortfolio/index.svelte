7dc7e3b148ee9352b1252976344b790bbb8352a1
dirkarnez <smalldirkalex@gmail.com>
2024-01-31 21:31:35 +0800

    - upload files

--- src/pages/StudentResources/LearningPortfolio/index.svelte
+++ src/pages/StudentResources/LearningPortfolio/index.svelte
@@ -1,3 +1,16 @@
 <script>
-    
+       const w = 200,
+        h = 200,
+        r = 100;
+
+    const fakeFetch = Promise.resolve(data = [{
+        name: "knowledge",
+        count: 13
+    }, {
+        name: "skill",
+        count: 14
+    }, {
+        name: "attitude",
+        count: 15
+    }]);
 </script>
