23f42c6083b10b1fc0129736298f4cff072dfbb1
dirkarnez <smalldirkalex@gmail.com>
2024-01-31 21:32:43 +0800

    - upload files

--- src/pages/StudentResources/LearningPortfolio/index.svelte
+++ src/pages/StudentResources/LearningPortfolio/index.svelte
@@ -1,14 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
 
-	let canvas;
-
-	onMount(() => {
-    });
-    const w = 200;
-    const h = 200;
-    const r = 100;
-
     const fakeFetch = Promise.resolve(data = [{
         name: "knowledge",
         count: 13
@@ -19,4 +11,16 @@
         name: "attitude",
         count: 15
     }]);
+
+    const w = 200;
+    const h = 200;
+    const r = 100;
+    
+	let canvas;
+
+	onMount(() => {
+    });
+
+
+
 </script>
