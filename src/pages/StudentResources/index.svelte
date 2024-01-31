9100ac92cb56e2ba62f88a86910e4374b663143d
dirkarnez <smalldirkalex@gmail.com>
2024-01-31 21:17:37 +0800

    - upload files

--- src/pages/StudentResources/index.svelte
+++ src/pages/StudentResources/index.svelte
@@ -147,27 +147,37 @@
 						
 							<h4>Class videos</h4>
 							<div class="columns is-multiline is-mobile">
-								{#each datum.videoGroups as videoGroup}
-									<div
-										class="column is-half-desktop is-half-tablet is-full-mobile"
-									>
-										{#each videoGroup as video}
-											<!-- <button on:click={() => {openModal = true}} style="border: none; margin-bottom: 0.75rem;background-color: white;">
-                                                <img src={`${video}`} style="width: 100%;"  alt="random img">
-                                            </button> -->
-											<div
-												class="overflow: hidden; padding-top: 56.25%; position: relative;"
-											>
-												<iframe
-													src={video}
-													title="YouTube video player"
-													frameborder="0"
-													style="border: none; width: 100%; height: 400px;"
-												></iframe>
-											</div>
-										{/each}
-									</div>
-								{/each}
+                                {#if Array.isArray(datum.videoGroups) && datum.videoGroups.length > 0 && datum.videoGroups.some((videoGroup) => Array.isArray(videoGroup) && videoGroup.length > 0)}
+                                    {#each datum.videoGroups as videoGroup}
+                                        <div
+                                            class="column is-half-desktop is-half-tablet is-full-mobile"
+                                        >
+                                            {#each videoGroup as video}
+                                                <!-- <button on:click={() => {openModal = true}} style="border: none; margin-bottom: 0.75rem;background-color: white;">
+                                                    <img src={`${video}`} style="width: 100%;"  alt="random img">
+                                                </button> -->
+                                                <div
+                                                    class="overflow: hidden; padding-top: 56.25%; position: relative;"
+                                                >
+                                                    <iframe
+                                                        src={video}
+                                                        title="YouTube video player"
+                                                        frameborder="0"
+                                                        style="border: none; width: 100%; height: 400px;"
+                                                    ></iframe>
+                                                </div>
+                                            {/each}
+                                        </div>
+                                    {/each}
+                                {:else}
+                                    <div
+                                        class="column is-half-desktop is-half-tablet is-full-mobile"
+                                    >
+                                        None
+                                    </div>
+
+                                {/if}
+
 							</div>
 						{/if}
 						<h4>Class photos</h4>
