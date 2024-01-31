a7c51a62bc57a98362ea8a0344b016910fd64873
dirkarnez <smalldirkalex@gmail.com>
2024-01-31 21:33:16 +0800

    - upload files

--- src/pages/StudentResources/LearningPortfolio/index.svelte
+++ src/pages/StudentResources/LearningPortfolio/index.svelte
@@ -19,7 +19,53 @@
 	let svgContainer;
 
 	onMount(() => {
-        
+        fakeFetch
+        .then(json => {
+            const width = 928;
+            const height = 500;
+            const marginTop = 20;
+            const marginRight = 0;
+            const marginBottom = 30;
+            const marginLeft = 40;
+            const x = d3.scaleBand()
+                .domain(d3.sort(data, d => -d.count).map(d => d.name))
+                .range([marginLeft, width - marginRight])
+                .padding(0.1);
+            const xAxis = d3.axisBottom(x).tickSizeOuter(0);
+            const y = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.count)]).nice()
+                .range([height - marginBottom, marginTop]);
+
+            //var target = d3.select(`#here`);
+
+            var svg = d3.select(`#here`)
+                //var svg = d3.create("svg")
+                .append("svg:svg")
+                .attr("viewBox", [0, 0, width, height])
+                .attr("width", width)
+                .attr("height", height)
+                .attr("style", "max-width: 100%; height: auto;")
+            svg.append("g")
+                .attr("class", "bars")
+                .attr("fill", "steelblue")
+                .selectAll("rect")
+                .data(data)
+                .join("rect")
+                .attr("x", d => x(d.name))
+                .attr("y", d => y(d.count))
+                .attr("height", d => y(0) - y(d.count))
+                .attr("width", x.bandwidth());
+
+            svg.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", `translate(0,${height - marginBottom})`)
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y-axis")
+                .attr("transform", `translate(${marginLeft},0)`)
+                .call(d3.axisLeft(y))
+                .call(g => g.select(".domain").remove());
     });
 </script>
 
