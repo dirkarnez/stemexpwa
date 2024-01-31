b3d09937e13d2b737ce716e3daf01974045e3fa3
dirkarnez <smalldirkalex@gmail.com>
2024-01-31 22:38:37 +0800

    - upload files

--- src/pages/StudentResources/LearningPortfolio/index.svelte
+++ src/pages/StudentResources/LearningPortfolio/index.svelte
@@ -72,12 +72,12 @@
     function a() {
 
         var fontDescriptors = {
-            Roboto: {
-                normal: 'fonts/Roboto-Regular.ttf',
-                bold: 'fonts/Roboto-Medium.ttf',
-                italics: 'fonts/Roboto-Italic.ttf',
-                bolditalics: 'fonts/Roboto-MediumItalic.ttf'
-            }
+            // Roboto: {
+            //     normal: 'fonts/Roboto-Regular.ttf',
+            //     bold: 'fonts/Roboto-Medium.ttf',
+            //     italics: 'fonts/Roboto-Italic.ttf',
+            //     bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+            // }
         };
         
         var printer = new PdfPrinter(fontDescriptors);
