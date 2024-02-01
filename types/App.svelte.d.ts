047c390d42283278b7dc0739cdc96b2777d74b3c
dirkarnez <smalldirkalex@gmail.com>
2024-02-01 19:39:40 +0800

    - upload files

---
+++ types/App.svelte.d.ts
@@ -0,0 +1,9 @@
+declare module <library exact name> {
+    export class <class name> {
+       constructor();
+ 
+       method(): void;
+    }
+ 
+    export default function (element: HTMLElement): string;
+ }
