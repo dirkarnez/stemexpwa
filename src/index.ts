63bff8b04fde187827b0ca1186d6550cb97d864c
dirkarnez <smalldirkalex@gmail.com>
2024-02-01 19:32:23 +0800

    - upload files

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import "./styles/index.scss";
 import App from "./App.svelte";
 // import App from "./Done.svelte";
 
-export default new App({
+export default new App<SvelteComponent>({
 	target: document.body,
 });
