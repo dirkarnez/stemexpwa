e9efbb203ddfbc474d30681689e3826ad175c385
dirkarnez <smalldirkalex@gmail.com>
2024-02-01 19:39:59 +0800

    - upload files

--- types/App.svelte.d.ts
+++ types/App.svelte.d.ts
@@ -1,9 +1,3 @@
 declare module "./App.svelte" {
-    export class <class name> {
-       constructor();
- 
-       method(): void;
-    }
- 
     export default function (element: HTMLElement): string;
  }
