1d6340ed6f73879b43b4f46527096798b97a2d60
dirkarnez <smalldirkalex@gmail.com>
2024-02-03 21:57:51 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -350,7 +350,7 @@
 					<div class="columns is-multiline is-mobile">
 						{#each $data.blog_entries || [] as blog_entry, index}
 									<div class="column is-quarter">
-										
+										A
 									</div>
 								<div class="column is-half">
 									<input
