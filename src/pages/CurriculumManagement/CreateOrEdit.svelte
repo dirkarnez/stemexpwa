796e20763229389c76bbf6b767f7096559babb46
dirkarnez <smalldirkalex@gmail.com>
2024-02-03 21:30:47 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -297,17 +297,17 @@
 			<div class="box">
 				<div class="field">
 					<label class="label">Curriculum plan
-					<div class="control">
-						<InputFileContainer>
-							<input
-								class="file-input"
-								type="file"
-								name="{information_entries_icon_file_field_key}"
-								on:change={e => handleImageChange(e, dataURI => setFields(`${information_entries_icon_file_preview_field_key}`, dataURI, true))}
-							/>
-						</InputFileContainer> 
-					</div>
-				</label>
+						<div class="control">
+							<InputFileContainer>
+								<input
+									class="file-input"
+									type="file"
+									name="{information_entries_icon_file_field_key}"
+									on:change={e => handleImageChange(e, dataURI => setFields(`${information_entries_icon_file_preview_field_key}`, dataURI, true))}
+								/>
+							</InputFileContainer> 
+						</div>
+					</label>
 				</div>
 			</div>
 		</section>
