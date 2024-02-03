684d418ee407d97986f7d3bcd8418a12bb6f6fc3
dirkarnez <smalldirkalex@gmail.com>
2024-02-03 21:21:47 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -126,7 +126,13 @@
 			const [  _wrappedFetchCurriculumEntry ] = WrappedFetch(`/api/curriculum?id=${id}&top-level=${!!parentId ? "false" : "true"}`);
 			debugger;
 			_wrappedFetchCurriculumEntry.then(_data => {
-				setFields(id_field_key, _data[id_field_key], true);
+				setFields(	let initialValues = {
+		email: '',
+		name: '',
+	}
+					
+				
+				id_field_key, _data[id_field_key], true);
 				setFields(description_field_key, _data[description_field_key], true);
 				setFields(icon_id_key, _data[icon_id_key], true);
 				setFields(parent_id_key, _data[parent_id_key], true);
