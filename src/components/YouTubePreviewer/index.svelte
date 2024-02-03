11065e40a2e795f7c60b81f36c02b6e98a25738f
dirkarnez <smalldirkalex@gmail.com>
2024-02-03 20:24:02 +0800

    - upload files

--- src/components/YouTubePreviewer/index.svelte
+++ src/components/YouTubePreviewer/index.svelte
@@ -43,9 +43,9 @@
 	}
 
 	function on() {
-		debugger;
-				videoTitle = player.getIframe().title;
-				debugger;
+	debugger;
+			videoTitle = player.getIframe().title;
+			debugger;
 	}
 
 	onMount(() => {
