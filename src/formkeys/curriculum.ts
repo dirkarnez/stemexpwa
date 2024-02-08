ed305f03bfbde65d142732d6faf8276ed3975ccf
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 18:39:18 +0800

    - upload files

---
+++ src/formkeys/curriculum.ts
@@ -0,0 +1,22 @@
+const description_field_key = "description";
+const icon_id_key = "icon_id";
+const parent_id_key = "parent_id";
+const icon_file_field_key = "icon_file";
+const icon_file_preview_field_key = "icon_file_preview";
+const id_field_key = "id";
+
+
+const youtube_video_entries_field_key = "youtube_video_entries";
+const youtube_video_entries_title_field_key = "title";
+const youtube_video_entries_url_field_key = "url";
+
+const information_entries_field_key = "information_entries";
+const information_entries_title_field_key = "title";
+const information_entries_icon_id_field_key = "icon_id";
+const information_entries_icon_file_field_key = "icon_file";
+const information_entries_icon_file_preview_field_key = "icon_file_preview";
+const information_entries_content_field_key = "content";
+
+const blog_entries_field_key = "blog_entries";
+const blog_entries_external_url_field_key = "external_url";
+const blog_entries_title_field_key = "title";
