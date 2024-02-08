5b943968b952881e47f1963b8567c4a46dac7428
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 19:09:21 +0800

    - upload files

--- src/pages/CurriculumManagement/index.svelte
+++ src/pages/CurriculumManagement/index.svelte
@@ -161,7 +161,7 @@
 			{/if}
 			<div class="column is-one-third-desktop is-half-tablet is-full-mobile">
 				<div style="margin-top: 1.5rem; position:relative;">
-					<div class="box"  style={`border: 2px dashed #0087F7; border-radius: 5px; background-color: unset;height: 96px; width: 100%;`}>
+					<div class="box" style={`border: 2px dashed #0087F7; border-radius: 5px; background-color: unset;height: 96px; width: 100%;`}>
 						<div class="content" >
 							<div class="is-flex is-align-items-center is-justify-content-center buttons">
 								<a class="button is-text" href={`${$location.pathname}/new-course`} use:link>add a new course here</a>
