1787c8c22033602227251ad691837ae4542128f4
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 18:25:57 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditType.svelte
+++ src/pages/CurriculumManagement/CreateOrEditType.svelte
@@ -1,4 +1,44 @@
 
+<script>
+	import { onMount, createEventDispatcher } from "svelte";
+	import { WrappedFetch, WrappedFetchPOST, WrappedFetchPOSTMultipart } from "../../utils/fetch";
+	import { stringToURLPart } from "../../utils/url";
+	import { useLocation, Link, Route, Router, link, navigate } from "svelte-routing";
+	// import SelectedCurriculumCategory from "./SelectedCurriculumCategory.svelte";
+	import { getResourcesAPIByID } from "../../utils/api";
+	import { handleImageChange } from "../../utils/file";
+	import { isNullOrEmpty } from "../../utils/strings";
+    import { createForm } from 'felte';
+	import InputFileContainer from "../../components/InputFileContainer.svelte";
+	import YouTubePreviewer from "../../components/YouTubePreviewer/index.svelte";
+	
+
+    //import { WrappedFetch } from "../../utils/fetch";
+
+    const description_field_key = "description";
+    const icon_id_key = "icon_id";
+	const parent_id_key = "parent_id";
+    const icon_file_field_key = "icon_file";
+    const icon_file_preview_field_key = "icon_file_preview";
+	const id_field_key = "id";
+
+
+	const youtube_video_entries_field_key = "youtube_video_entries";
+	const youtube_video_entries_title_field_key = "title";
+	const youtube_video_entries_url_field_key = "url";
+
+	const information_entries_field_key = "information_entries";
+	const information_entries_title_field_key = "title";
+	const information_entries_icon_id_field_key = "icon_id";
+	const information_entries_icon_file_field_key = "icon_file";
+	const information_entries_icon_file_preview_field_key = "icon_file_preview";
+	const information_entries_content_field_key = "content";
+
+	const blog_entries_field_key = "blog_entries";
+	const blog_entries_external_url_field_key = "external_url";
+	const blog_entries_title_field_key = "title";
+
+</script>
 <form use:form enctype="multipart/form-data">
 
     <div class="field">
