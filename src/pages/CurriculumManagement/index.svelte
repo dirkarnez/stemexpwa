21ca68581cecfe9e182057318bd21501c6bcb59b
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 19:01:54 +0800

    - upload files

--- src/pages/CurriculumManagement/index.svelte
+++ src/pages/CurriculumManagement/index.svelte
@@ -131,7 +131,7 @@
 									<div class="box" style={`background-color: ${colors[index % colors.length]};height: 96px; width: 100%;`}>
 										<div class="columns m-0 p-0">
 											<div class="column is-narrow-desktop m-0 p-0">
-												<img src={getResourcesAPIByID(icon_id)} style="max-height: 96px;" alt="">
+												<img src={getResourcesAPIByID(icon_id)} style={`max-height: 96px;`} alt="">
 											</div>
 											<div class="column is-full-mobile">
 												<div class="content">
