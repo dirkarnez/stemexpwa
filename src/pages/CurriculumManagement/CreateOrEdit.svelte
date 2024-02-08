085574c324ef9875d9046386e547f95c8c623622
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 20:48:27 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -330,11 +330,11 @@
 					{#each $data.blog_entries || [] as blog_entry, index}
 								<div class="column is-half">
 									<input
-														class="input"
-														type="text"
-														placeholder="URL of the video"
-														required={true}
-													/>
+										class="input"
+										type="text"
+										placeholder="URL of the video"
+										required={true}
+									/>
 								</div>
 							<div class="column is-half">
 								<input
