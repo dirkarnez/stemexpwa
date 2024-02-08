2e73170a0ddbbfa8dac7d6e8f7078acbaffac2f1
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 19:27:39 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -214,52 +214,7 @@
 	</div>
 </div> -->
 
-<form use:form enctype="multipart/form-data">
-	<input
-		class="input"
-		type="hidden"
-		name={parent_id_key}
-		bind:value={$data[parent_id_key]}
-	/>
-	
-	<div class="field">
-		<figure class="image is-128x128">
-			<img
-				src={$data[icon_id_key]
-					? getResourcesAPIByID($data[icon_id_key])
-					: 
-					$data[icon_file_preview_field_key]
-					? $data[icon_file_preview_field_key]
-					: `https://bulma.io/images/placeholders/128x128.png`}
-				alt=""
-			/>
-		</figure>
-		<label class="label">Icon
-			<div class="control">
-				<InputFileContainer>
-					<input
-						class="file-input"
-						type="file"
-						name={icon_file_field_key}
-						on:change={e => handleImageChange(e, dataURI => setFields(icon_file_preview_field_key , dataURI, true))}
-					/>
-				</InputFileContainer>
-			</div>
-		</label>
-	</div>
-	<div class="field">
-		<label class="label">Description
-			<div class="control">
-				<input
-					class="input"
-					type="text"
-					name={description_field_key}
-					bind:value={$data[description_field_key]}
-					required={true}
-				/>
-			</div>
-		</label>
-	</div>
+
 
 	<!-- <div class="field">
 		<label class="label" for="select-accessible">Accessible to
@@ -279,6 +234,60 @@
 
 		<section class="hero">
 			<div class="box">
+				<form use:form enctype="multipart/form-data">
+					<div class="field">
+						<label class="label">Description
+							<div class="control">
+								<input
+									class="input"
+									type="hidden"
+									name={parent_id_key}
+									bind:value={$data[parent_id_key]}
+								/>
+							</div>
+						</label>
+					</div>
+				
+					
+					<div class="field">
+						<figure class="image is-128x128">
+							<img
+								src={$data[icon_id_key]
+									? getResourcesAPIByID($data[icon_id_key])
+									: 
+									$data[icon_file_preview_field_key]
+									? $data[icon_file_preview_field_key]
+									: `https://bulma.io/images/placeholders/128x128.png`}
+								alt=""
+							/>
+						</figure>
+						<label class="label">Icon
+							<div class="control">
+								<InputFileContainer>
+									<input
+										class="file-input"
+										type="file"
+										name={icon_file_field_key}
+										on:change={e => handleImageChange(e, dataURI => setFields(icon_file_preview_field_key , dataURI, true))}
+									/>
+								</InputFileContainer>
+							</div>
+						</label>
+					</div>
+					<div class="field">
+						<label class="label">Description
+							<div class="control">
+								<input
+									class="input"
+									type="text"
+									name={description_field_key}
+									bind:value={$data[description_field_key]}
+									required={true}
+								/>
+							</div>
+						</label>
+					</div>
+
 				<div class="field">
 					<label class="label">Curriculum plan
 						<div class="control">
@@ -459,6 +468,12 @@
 					  </div>
 				</div>
 			</div>
+			<div class="field is-grouped">
+				<div class="control">
+					<button class="button is-primary">Save</button>
+				</div>
+			</div>
+		</form>
 		</section>
 <!-- 	
 		<section class="hero">
@@ -645,12 +660,7 @@
 			</button>
 		</section> -->
 
-	<div class="field is-grouped">
-		<div class="control">
-			<button class="button is-primary">Save</button>
-		</div>
-	</div>
-</form>
+
 
 <style>
 	.hero {
