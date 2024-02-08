2d93e3423e28667ab550d6c810e6d17323f30bcb
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 20:52:08 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -320,7 +320,7 @@
 			<div class="field">
 				<p class="label">YouTube videos</p>
 				<div class="columns is-multiline is-mobile">
-					{#each $data.youtube_video_entries || [] as youtube_video_entry, index}
+					{#each $data[youtube_video_entries_field_key] || [] as youtube_video_entry, index}
 						<YouTubePreviewer bind:videoURL={youtube_video_entry.url}/>
 					{/each}
 					<div class="column">
