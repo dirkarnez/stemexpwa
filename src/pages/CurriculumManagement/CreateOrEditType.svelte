fc618cd1a95040ce2712b187d9eaff7e1881b4ce
dirkarnez <smalldirkalex@gmail.com>
2024-02-14 19:43:04 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditType.svelte
+++ src/pages/CurriculumManagement/CreateOrEditType.svelte
@@ -72,6 +72,15 @@
                 debugger;
                 setFields(initValues);
             });
+        } else {
+            const initValues = {
+                    [curriculumFormKeys.id_key]: _data[curriculumFormKeys.id_key],
+                    [curriculumFormKeys.description_key]: _data[curriculumFormKeys.description_key],
+                    [curriculumFormKeys.icon_id_key]: _data[curriculumFormKeys.icon_id_key],
+                    [curriculumFormKeys.parent_id_key]: _data[curriculumFormKeys.parent_id_key]
+                };
+                debugger;
+                setFields(initValues);
         }
 
     });
