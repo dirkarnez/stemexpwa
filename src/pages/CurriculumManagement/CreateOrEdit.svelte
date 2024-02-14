b1e1736f79d48a2db32525d083f895a63407f15f
dirkarnez <smalldirkalex@gmail.com>
2024-02-14 20:52:04 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -417,23 +417,32 @@
 															x
 														</button>
 													</div>
-													<InputFileContainer>
-														<input
-															class="file-input"
-															type="file"
-															name="{information_entries_icon_file_field_key}"
-															on:change={e => handleImageChange(e, dataURI => setFields(`${information_entries_icon_file_preview_field_key}`, dataURI, true))}
-														/>
-													</InputFileContainer> 
-													<InputFileContainer>
-														<input
-															class="file-input"
-															type="file"
-															name="{information_entries_icon_file_field_key}"
-															on:change={e => handleImageChange(e, dataURI => setFields(`${information_entries_icon_file_preview_field_key}`, dataURI, true))}
-														/>
-													</InputFileContainer> 
-
+													<div>
+														<InputFileContainer>
+															<input
+																class="file-input"
+																type="file"
+																name="{information_entries_icon_file_field_key}"
+																on:change={e => handleImageChange(e, dataURI => setFields(`${information_entries_icon_file_preview_field_key}`, dataURI, true))}
+															/>
+														</InputFileContainer> 
+														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
+															x
+														</button>
+													</div>
+													<div>
+														<InputFileContainer>
+															<input
+																class="file-input"
+																type="file"
+																name="{information_entries_icon_file_field_key}"
+																on:change={e => handleImageChange(e, dataURI => setFields(`${information_entries_icon_file_preview_field_key}`, dataURI, true))}
+															/>
+														</InputFileContainer> 
+														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
+															x
+														</button>
+													</div>
 												</div>
 												<div class="column is-one-quarter"  style="border-bottom: 1px solid hsl(0deg,0%,86%);">
 													Student notes
