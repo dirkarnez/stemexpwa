ba16989eba6cbf88cb5f56d7b8f045a75ec82901
dirkarnez <smalldirkalex@gmail.com>
2024-02-14 19:54:28 +0800

    - upload files

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,11 +1,11 @@
-export const handleImageChange = <K extends keyof HTMLElementEventMap>(event: HTMLElementEventMap[K] & { target: HTMLInputElement }, callback: (result: string | ArrayBuffer | null) => void) => {
+export const handleImageChange = <K extends keyof HTMLElementEventMap>(event: HTMLElementEventMap[K] & { target: HTMLInputElement }, callback: (result: string | ArrayBuffer | null, filename: string) => void) => {
     if (!!event.target.files && event.target.files.length == 1) {
         const file: File =  event.target.files[0];
         if (file) {
             const reader = new FileReader();
 
             reader.onload = (event) => {
-                callback(!!event.target ? event.target.result : null);
+                callback(!!event.target ? event.target.result : null, file.name);
             };
     
             reader.readAsDataURL(file);
