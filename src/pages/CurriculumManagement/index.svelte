9b3a2cfc86eeaf9a7adf26b97107722235dd52c1
dirkarnez <smalldirkalex@gmail.com>
2024-02-15 21:42:48 +0800

    - upload files

--- src/pages/CurriculumManagement/index.svelte
+++ src/pages/CurriculumManagement/index.svelte
@@ -44,27 +44,6 @@
 
 	onMount(init);
 </script>
-<style>
-	.my-temp {
-	  animation: fadeInAnimation ease 2s;
-	  animation-iteration-count: 1;
-	  animation-fill-mode: forwards;
-	}
-	
-	
-	@keyframes fadeInAnimation {
-	  0% {
-		  opacity: 0;
-	  }
-	
-	  100% {
-		  opacity: 1;
-	  }
-	}
-	</style>
-<div class="my-temp">
-
-
 <Router>
 	{#if Array.isArray(curriculum)}
 		{#each curriculum as { description, icon_id, id, parent_id, is_course } }
@@ -244,4 +223,3 @@
 		</div>
 	</Route>
 </Router>
-</div>
