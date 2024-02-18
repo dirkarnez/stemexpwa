86f94c458c5976fde7ba6baca9766ef9b1db0091
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 20:21:18 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -225,7 +225,8 @@
 		return () => addField(`${curriculumFormKeys.levels_key}`, [{
 			[curriculumFormKeys.id_key]: "",
 			[curriculumFormKeys.lessons_key]: [{
-
+				[curriculumFormKeys.blog_entries_external_url_key]: "", 
+				[curriculumFormKeys.blog_entries_title_key]: ""
 			}]
 		}], index);
 	}
