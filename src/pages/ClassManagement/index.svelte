37874989d30518849d4e1120ea09716da298a30e
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 03:45:34 +0800

    - upload files

--- src/pages/ClassManagement/index.svelte
+++ src/pages/ClassManagement/index.svelte
@@ -30,7 +30,7 @@
 	// // export let previousPath;
 
 	// const init = () => {
-	// 	const [  _wrappedFetchCurriculum ] = WrappedFetch(isNullOrEmpty(parentId) ? `/api/curriculum?top-level=true` : `/api/curriculum-courses?parent-id=${parentId}`);
+	// 	const [  _wrappedFetchCurriculum ] = WrappedFetch(isNullOrEmpty(parentId) ? `/api/curriculum?top-level=true` : `/api/`);
 	// 	wrappedFetchCurriculum = _wrappedFetchCurriculum;
 	// 	wrappedFetchCurriculum.then(data => {
 	// 		curriculum = data;
