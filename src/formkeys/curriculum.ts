e72995e9eae50a49b9bd753bb72c8d1602dc5f7d
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 19:44:42 +0800

    - upload files

--- src/formkeys/curriculum.ts
+++ src/formkeys/curriculum.ts
@@ -10,12 +10,7 @@ export const youtube_video_entries_key = "youtube_video_entries";
 export const youtube_video_entries_title_key = "title";
 export const youtube_video_entries_url_key = "url";
 
-export const information_entries_key = "information_entries";
-export const information_entries_title_key = "title";
-export const information_entries_icon_id_key = "icon_id";
-export const information_entries_icon_file_key = "icon_file";
-export const information_entries_icon_file_preview_key = "icon_file_preview";
-export const information_entries_content_key = "content";
+
 
 export const blog_entries_key = "blog_entries";
 export const blog_entries_external_url_key = "external_url";
@@ -23,3 +18,12 @@ export const blog_entries_title_key = "title";
 
 
 export const levels_key = "levels";
+export const lessons_key = "levels";
+
+
+export const information_entries_key = "information_entries";
+export const information_entries_title_key = "title";
+export const information_entries_icon_id_key = "icon_id";
+export const information_entries_icon_file_key = "icon_file";
+export const information_entries_icon_file_preview_key = "icon_file_preview";
+export const information_entries_content_key = "content";
