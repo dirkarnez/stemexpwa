0bf807d4a5c7e627364fedabb8a8942cd58468b7
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 06:31:50 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -713,7 +713,7 @@
 																</div>
 															{/each}
 															<div>
-																<InputFileContainer filename={$data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key][curriculumFormKeys.lesson_presentation_note_file_name_key]}>
+																<InputFileContainer filename={$data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key][curriculumFormKeys.lesson_teacher_note_file_name_key]}>
 																	<input
 																		class="file-input"
 																		type="file"
