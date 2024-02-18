47601070438d0fc346d065ff9f38534625429654
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 21:34:42 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -572,7 +572,7 @@
 																<input
 																	class="file-input"
 																	type="file"
-																	name="{curriculumFormKeys.information_entries_icon_file_key}"
+																	name={curriculumFormKeys.information_entries_icon_file_key}
 																	on:change={e => handleImageChange(e, dataURI => setFields(`${curriculumFormKeys.information_entries_icon_file_preview_key}`, dataURI, true))}
 																/>
 															</InputFileContainer> 
