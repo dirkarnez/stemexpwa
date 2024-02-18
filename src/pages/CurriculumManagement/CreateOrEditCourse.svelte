4e970d06f46ce741b0c51b097c5fbeaac1938edb
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 06:25:11 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -712,7 +712,7 @@
 															Misc. teaching materials<br/>(e.g. source code)
 														</div>
 														<div class="column is-three-quarters"  style="border-bottom: 1px solid hsl(0deg,0%,86%);">
-															{#each ($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key] || []) as teacherNote, teacherNoteIndex}
+															{#each ($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_misc_materials_key] || []) as teacherNote, teacherNoteIndex}
 																<div>
 																	<a href="google.com" class="is-underlined">{teacherNote[curriculumFormKeys.lesson_teacher_note_file_name_key]}</a>
 																	<button type="button" class="is-danger button delete" on:click={removeMiscMaterial(levelIndex, lessonIndex, teacherNoteIndex)}>
