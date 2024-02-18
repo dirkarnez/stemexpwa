03e0142bb9e96be94ed970ec9df2a70b6f2a5ab5
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 06:24:21 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -688,9 +688,9 @@
 															Teacher notes
 														</div>
 														<div class="column is-three-quarters"  style="border-bottom: 1px solid hsl(0deg,0%,86%);">
-															{#each ($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key] || []) as studentNote, studentNoteIndex}
+															{#each ($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key] || []) as teacherNote, teacherNoteIndex}
 																<div>
-																	<a href="google.com" class="is-underlined">{studentNote[curriculumFormKeys.lesson_student_note_file_name_key]}</a>
+																	<a href="google.com" class="is-underlined">{teacherNote[curriculumFormKeys.lesson_student_note_file_name_key]}</a>
 																	<button type="button" class="is-danger button delete" on:click={removeStudentNote(levelIndex, lessonIndex, studentNoteIndex)}>
 																		x
 																	</button>
