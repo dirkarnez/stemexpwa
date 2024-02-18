86d2a80a203f3c288e566afe130e7f9080b09995
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 04:10:25 +0800

    - upload files

--- src/components/YouTubePreviewer/index.svelte
+++ src/components/YouTubePreviewer/index.svelte
@@ -77,14 +77,7 @@
 </style>
 
 <div class="column is-three-quarters">
-	<input
-		class="input"
-		type="text"
-		name={name}
-		bind:value={videoURL}
-		placeholder="URL of the video"
-		required={true}
-	/>
+
 	<slot/>
 </div>
 <div class="column is-one-quarter">
