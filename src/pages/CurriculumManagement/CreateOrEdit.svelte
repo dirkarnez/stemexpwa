ac584866286e8d7377862e0bb4c5ec843905f0a5
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 22:24:36 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -6,6 +6,7 @@
 		WrappedFetchPOSTMultipart,
 	} from "../../utils/fetch";
 	import { stringToURLPart } from "../../utils/url";
+	import { HasAtLeastOneItemInArray } from "../../utils/array";
 	import {
 		useLocation,
 		Link,
