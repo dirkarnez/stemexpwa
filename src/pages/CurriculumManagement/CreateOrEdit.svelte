179925e9bc4ff63912ba0df6894e8fc2ff46a065
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 22:39:53 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -50,7 +50,7 @@
 		levels.forEach(level => {
 			const lessons = level[curriculumFormKeys.lessons_key];
 			if (!hasAtLeastOneItemInArray(lessons)) {
-				throw new Error(`Not OK: Please add at least 1 lesson at every level`);
+				throw new Error(`Not OK: Please add at least 1 lesson at every course level`);
 			}
 
 			const presentationNotes = lessons[curriculumFormKeys.lesson_presentation_notes_key];
