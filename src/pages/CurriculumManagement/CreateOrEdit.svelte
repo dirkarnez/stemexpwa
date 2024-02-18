e77c2cfa7a02f10bb8335d53ebf0192305d463c0
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 22:42:04 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -63,7 +63,7 @@
 				if (!hasAtLeastOneItemInArray(lesson[curriculumFormKeys.lesson_teacher_notes_key])) {
 					throw new Error(`Not OK: Please add at least 1 presentation notes at every lesson`);
 				}
-				if (!hasAtLeastOneItemInArray(lesson[curriculumFormKeys.lesson_student_notes_key])) {
+				if (!hasAtLeastOneItemInArray(lesson[curriculumFormKeys.lesson_misc_materials_key])) {
 					throw new Error(`Not OK: Please add at least 1 presentation notes at every lesson`);
 				}
 
