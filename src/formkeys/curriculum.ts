a3672eb861fe91fc4ee969cd513389ab6af41f08
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 05:26:38 +0800

    - upload files

--- src/formkeys/curriculum.ts
+++ src/formkeys/curriculum.ts
@@ -25,6 +25,7 @@ export const levels_key = "levels";
 export const level_name_key = "name";
 
 export const lesson_presentation_notes_key = "presentation_notes";
+export const lesson_presentation_notes_file_name_key = "name"
 export const lesson_student_notes_key = "student_notes";
 export const lesson_teacher_notes_key = "teacher_notes";
 export const lesson_misc_materials_key = "misc_materials";
