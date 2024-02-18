22a92b2639723f80fced6f5e2f613adf9bbbe384
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 06:22:29 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -299,6 +299,14 @@
 	}
 
 
+	function removeMiscMaterial(levelIndex, lessonIndex, miscMaterialIndex) {
+		return () => unsetField(`${curriculumFormKeys.levels_key}.${levelIndex}.${curriculumFormKeys.lessons_key}.${lessonIndex}.${curriculumFormKeys.lesson_teacher_notes_key}.${miscMaterialIndex}`);
+	}
+
+
+	lesson_misc_materials_key
+
+
 	// function removeInformationEntry(index) {
 	// 	return () => unsetField(`${curriculumFormKeys.information_entries_key}.${index}`);
 	// }
