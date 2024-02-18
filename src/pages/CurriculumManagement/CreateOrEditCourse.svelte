74e144a32dbcc2e14eccd94ba987d06d6dfd6cbb
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 04:41:38 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -696,52 +696,7 @@
 													</div>
 												</div>
 											{/each}
-					
-					
-											<!-- <div class="column is-one-quarter" style="background-color: hsl(0deg,0%,95%);">
-												Lesson 2
-											</div>
-											<div class="column  is-three-quarters" style="background-color: hsl(0deg,0%,95%);">
-												<div class="columns is-multiline is-mobile">
-													<div class="column is-one-quarter" style="border-bottom: 1px solid hsl(0deg,0%,86%);">
-														Presentation notes
-													</div>
-													<div class="column is-three-quarters"  style="border-bottom: 1px solid hsl(0deg,0%,86%);">
-														<a href="google.com" class="is-underlined">App Inventor Introductory [L2 -HelloPurr].pptx</a>
-														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
-															x
-														</button>
-													</div>
-													<div class="column is-one-quarter" style="border-bottom: 1px solid hsl(0deg,0%,86%);">
-														Student notes
-													</div>
-													<div class="column is-three-quarters" style="border-bottom: 1px solid hsl(0deg,0%,86%);">
-														<a href="google.com" class="is-underlined">App Inventor Intro _Lesson2_Student Notes.pdf</a>
-														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
-															x
-														</button>
-													</div>
-													<div class="column is-one-quarter">
-														Misc. teaching materials<br/>(e.g. source code)
-													</div>
-													<div class="column is-three-quarters">
-														<a href="google.com" class="is-underlined">HelloPurr.aia</a>
-														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
-															x
-														</button>
-														<br/>
-														<a href="google.com" class="is-underlined">kitty.png</a>
-														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
-															x
-														</button>
-														<br/>
-														<a href="google.com" class="is-underlined">meow.mp3</a>
-														<button type="button" class="is-danger button delete" on:click={removeBlogEntry(0)}>
-															x
-														</button>
-													</div>
-												</div>
-											</div> -->
+											
 											<div class="column is-full">
 												<button type="button" class="button is-primary is-light" style="width: 100%;" on:click={addLesson(levelIndex, ($data[curriculumFormKeys.lessons_key] || []).length)}>
 													Add new lesson
