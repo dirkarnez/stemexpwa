af1d5456d358db1b0e332b7bb8047c9d4ec0fd06
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 22:24:00 +0800

    - upload files

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -6,7 +6,7 @@ export const splitArrayToArrayGroups = <T>(array: Array<T>, numOfGroups: number)
     Array(numOfGroups).fill([]),
 );
 
-export const atLeastOneItemInArray =  <T>(array: Array<T>) => {
+export const HasAtLeastOneItemInArray =  <T>(array: Array<T>) => {
     return !Array.isArray(array) 
     ||  values[curriculumFormKeys.youtube_video_entries_key].length < 1
 }
