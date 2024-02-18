03ed5b56a941a5dc31d1c1ae326fc7678a1b0ed8
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 20:22:18 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEdit.svelte
+++ src/pages/CurriculumManagement/CreateOrEdit.svelte
@@ -420,7 +420,7 @@
 				<p class="label">Levels</p>
 				<div class="columns is-multiline is-mobile">
 					<!-- !!!!!!!!TODO -->
-					{#each ($data[curriculumFormKeys.levels_key] || []) as level, levelIndex}
+					{#each ($data[curriculumFormKeys.levels_key] || []) as { name }, levelIndex}
 							<div class="column is-full">
 								<div class="box">
 									<div class="columns">
