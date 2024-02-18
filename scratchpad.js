d9471c5ad1785075cc210afe0df4fc5f0e73aba9
dirkarnez <smalldirkalex@gmail.com>
2024-02-18 21:34:13 +0800

    - upload files

--- scratchpad.js
+++ scratchpad.js
@@ -5,3 +5,40 @@ CurriculumEntry -> new table (CurriculumPlan)
 
 
 information_entries
+
+{
+    "id": "",
+    "description": "",
+    "icon_id": "",
+    "parent_id": "",
+    "youtube_video_entries": "",
+    "curriculum_plan_id": "",
+    "blog_entries": [{
+      "title": "",
+      "external_url": ""
+    }],
+    "levels": [{
+      "id": "",
+      "name": "",
+      "icon_id": "",
+      "description": "",
+      "lessons": [{
+        "presentation_notes": [{
+           "id": "",
+          "resourse_id": ""
+        }],
+        "student_notes": [{
+           "id": "",
+          "resourse_id": ""
+        }],
+        "teacher_notes": [{
+           "id": "",
+          "resourse_id": ""
+        }],
+        "misc_materials": [{
+           "id": "",
+          "resourse_id": ""
+        }]
+      }]
+    }]
+  }
