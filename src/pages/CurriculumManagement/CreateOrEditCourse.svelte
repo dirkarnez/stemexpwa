40aaf745d0b6804994eb8e9e95db01aafdc2b674
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 15:58:05 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -886,7 +886,7 @@
 																		class="input"
 																		type="hidden"
 																		style="display: none;"
-																		name="{curriculumFormKeys.levels_key}.{levelIndex}.{curriculumFormKeys.lessons_key}.{lessonIndex}.{curriculumFormKeys.lesson_presentation_notes_key}.{miscMaterialIndex}.{curriculumFormKeys.lesson_teacher_note_file_id_key}"
+																		name="{curriculumFormKeys.levels_key}.{levelIndex}.{curriculumFormKeys.lessons_key}.{lessonIndex}.{curriculumFormKeys.lesson_presentation_notes_key}.{miscMaterialIndex}.{curriculumFormKeys.lesson_misc_materials_file_id_key}"
 																	/>
 																	<input
 																		class="input"
