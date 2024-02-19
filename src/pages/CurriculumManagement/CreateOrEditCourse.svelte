4be861bffe68728d0d94dedb3736421c23258114
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 11:23:23 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -100,7 +100,8 @@
 				}
 				
 				newIndexedArray(lessonLength).forEach(j => {
-					if (value[curriculumFormKeys.levels_key][i][curriculumFormKeys.lessons_key][j][curriculumFormKeys.lesson_presentation_notes_key].length < 1) {
+					const presentationNotesLength = value[curriculumFormKeys.levels_key][i][curriculumFormKeys.lessons_key][j][curriculumFormKeys.lesson_presentation_notes_key]
+					if (presentationNotesLength.length < 1) {
 						rj(new Error(`Not OK: Please add at least 1 presentation notes for every course level`));
 						return;
 					} else
