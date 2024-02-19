0e95112c7fe00f70a77b5272465513a5ad182cad
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 08:14:17 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -651,12 +651,12 @@
 																	<input
 																		class="file-input"
 																		type="file"
-																		name="{curriculumFormKeys.levels_key}.{levelIndex}.{curriculumFormKeys.lessons_key}.{lessonIndex}.{curriculumFormKeys.lesson_presentation_notes_key}.{($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_presentation_notes_key] || []).length}.{curriculumFormKeys.lesson_presentation_note_file_key}"
+																		name="{curriculumFormKeys.levels_key}.{levelIndex}.{curriculumFormKeys.lessons_key}.{lessonIndex}.{curriculumFormKeys.lesson_presentation_notes_key}.{($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_presentation_notes_key] || []).length - 1}.{curriculumFormKeys.lesson_presentation_note_file_key}"
 																		multiple={false}
 																		required={!(($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_presentation_notes_key] || []).length > 0)}
 																		on:change={e => 
 																			handleDocumentChange(e, (file, filename) => {
-																				const index = ($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_presentation_notes_key] || []).length;
+																				const index = ($data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_presentation_notes_key] || []).length - 1;
 																				debugger;
 																				setFields(
 																					`${curriculumFormKeys.levels_key}.${levelIndex}.${curriculumFormKeys.lessons_key}.${lessonIndex}.${curriculumFormKeys.lesson_presentation_notes_key}.${index}.${curriculumFormKeys.lesson_presentation_note_file_key}`,
