28a71626183ebb1d94d74b0c06750024da96cabc
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 08:48:09 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -734,7 +734,7 @@
 																		class="input"
 																		type="hidden"
 																		name="{curriculumFormKeys.levels_key}.{levelIndex}.{curriculumFormKeys.lessons_key}.{lessonIndex}.{curriculumFormKeys.lesson_teacher_notes_key}.{teacherNote}.{curriculumFormKeys.lesson_teacher_note_file_key}"
-																		bind:value={$data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key][teacherNote][curriculumFormKeys.lesson_teacher_note_file_key]}
+																		bind:value={$data[curriculumFormKeys.levels_key][levelIndex][curriculumFormKeys.lessons_key][lessonIndex][curriculumFormKeys.lesson_teacher_notes_key][teacherNoteIndex][curriculumFormKeys.lesson_teacher_note_file_key]}
 																	/>
 																	<a href={`javascript:void(0);`} class="is-underlined">{teacherNote[curriculumFormKeys.lesson_teacher_note_file_name_key] || "" }</a>
 																	<button type="button" class="is-danger button delete" on:click={removeTeacherNote(levelIndex, lessonIndex, teacherNoteIndex)}>
