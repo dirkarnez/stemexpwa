5703a6d12c1f65f9e5563bc08eac6879c539f815
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 11:37:57 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -156,7 +156,7 @@
 							`${curriculumFormKeys.levels_key}.${i}.${curriculumFormKeys.lessons_key}.${j}.${curriculumFormKeys.lesson_misc_materials_key}.${k}.${curriculumFormKeys.lesson_misc_materials_file_key}`, 
 							new File(
 								[formData.get(`${curriculumFormKeys.levels_key}.${i}.${curriculumFormKeys.lessons_key}.${j}.${curriculumFormKeys.lesson_misc_materials_key}.${k}.${curriculumFormKeys.lesson_misc_materials_file_key}`)], 
-								value[curriculumFormKeys.levels_key][i][curriculumFormKeys.lessons_key][i][curriculumFormKeys.lesson_misc_materials_key][k][curriculumFormKeys.lesson_teacher_note_file_name_key]
+								value[curriculumFormKeys.levels_key][i][curriculumFormKeys.lessons_key][i][curriculumFormKeys.lesson_misc_materials_key][k][curriculumFormKeys.lesson_misc_materials_file_name_key]
 							)
 						);
 					});
