c58408761496e104881521296fc43520259020a2
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 11:24:34 +0800

    - upload files

--- src/pages/CurriculumManagement/CreateOrEditCourse.svelte
+++ src/pages/CurriculumManagement/CreateOrEditCourse.svelte
@@ -109,7 +109,7 @@
 						newIndexedArray(presentationNotesLength).forEach(k => {
 							formData.set(
 								`${curriculumFormKeys.levels_key}.${i}.${curriculumFormKeys.lessons_key}.${j}.${curriculumFormKeys.lesson_presentation_notes_key}.${k}.${lesson_presentation_note_file_key}`, 
-								new File([formData.get('levels.0.lessons.0.presentation_notes.0.presentation_note_file')], "345")
+								new File([formData.get(${curriculumFormKeys.levels_key}.${i}.${curriculumFormKeys.lessons_key}.${j}.${curriculumFormKeys.lesson_presentation_notes_key}.${k}.${lesson_presentation_note_file_key})], "345")
 							);
 						})
 
