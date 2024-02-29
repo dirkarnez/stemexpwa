1113420b8b53f6a07448f8be6dceafa425a6fab1
dirkarnez <smalldirkalex@gmail.com>
2024-02-29 19:09:34 +0800

    - wip

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,8 @@
 	import Home from "./Home.svelte";
 	import LoginPage from "./pages/Login/index.svelte";
 	import RegisterPage from "./pages/Register/index.svelte";
-	
+	import ActivationPage from "./pages/Activation/index.svelte";
+
 </script>
 
 <Router>
@@ -12,6 +13,9 @@
 	</Route>
 	<Route path="/register">
 		<RegisterPage/>
+	</Route>
+	<Route path="/activation">
+		<ActivationPage/>
 	</Route>
 	<Route path="/*">
 		<Home/>
---
+++ src/pages/Activation/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Router, Route, useLocation } from "svelte-routing";
+    import queryString from 'query-string';
+	import { onMount } from "svelte";
+	let location = useLocation();
+    const key = queryString.parse($location.search)["key"] || "";
+
+    onMount(() => {
+        fetch(`/api/${key}`)
+    })
+</script>
+
+<h1>{key}</h1>
