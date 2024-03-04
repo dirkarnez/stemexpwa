7cd8958d6de9ca21063a8d428b581d2c419af605
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 18:37:44 +0800

    - upload files

--- src/pages/Register/index.svelte
+++ src/pages/Register/index.svelte
@@ -9,18 +9,19 @@
 	
 	const { form } = createForm({
 		onSubmit: (values) => {
-				const [  wrappedFetchPromise , abort ] = WrappedFetch("/api/login", {
-					method: "POST",
-					body: JSON.stringify(values)
-				});
+			alert("Will be available");
+				// const [  wrappedFetchPromise , abort ] = WrappedFetch("/api/register", {
+				// 	method: "POST",
+				// 	body: JSON.stringify(values)
+				// });
 
-				wrappedFetchPromise
-				.then(() => {
-					navigate("/", { replace: true });
-				})
-				.catch(e => {
-					alert("fetch error: " + "cannot login");
-				});
+				// wrappedFetchPromise
+				// .then(() => {
+				// 	navigate("/", { replace: true });
+				// })
+				// .catch(e => {
+				// 	alert("fetch error: " + "cannot login");
+				// });
 		}
 	})
 
