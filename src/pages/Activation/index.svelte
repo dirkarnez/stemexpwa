e93d4922f5290258a1ea724a0812e88b3b11b1d8
dirkarnez <smalldirkalex@gmail.com>
2024-03-10 21:50:58 +0800

    - upload files

--- src/pages/Activation/index.svelte
+++ src/pages/Activation/index.svelte
@@ -4,7 +4,7 @@
     import { WrappedFetch } from "../../utils/fetch";
 	import { onMount } from "svelte";
     import HundredVh from "../../components/HundredVh/index.svelte";
-    
+
 	let location = useLocation();
     const key = queryString.parse($location.search)["key"] || "";
 
@@ -26,4 +26,83 @@
 	<h2 slot="title" class="subtitle" style="color: white">
 		Sign in to your account to continue
 	</h2>
+    <div slot="content" class="card" style="text-align: left;">
+		<div class="card-content" style="padding: 4rem 2.5rem;">
+			<div class="content">
+				<div style="text-align: center;">
+					<img
+						src={stemex_icon}
+						alt="stemex_icon"
+						width="132"
+						height="132"
+						class="img-fluid" />
+				</div>
+				<form use:form>
+					<div class="field">
+						<label class="label" for="userName">User name</label>
+						<div class="control has-icons-left">
+							<!-- {#if $myForm.hasError('user_name.required')}
+								<input
+									id="b"
+									class="input is-danger"
+									type="text"
+									placeholder="Enter your user name"
+									bind:value={$userName.value} />
+							{:else} -->
+								<input
+									id="userName"
+									class="input"
+									name="user_name"
+									type="text"
+									placeholder="Enter your user name"/>
+							<!-- {/if} -->
+							<span class="icon is-small is-left">
+								<i class="fas fa-user" />
+							</span>
+						</div>
+						<!-- {#if $myForm.hasError('user_name.required')}
+							<p class="help is-danger">User Name is required</p>
+						{/if} -->
+					</div>
+					<div class="field">
+						<label class="label" for="password">Password</label>
+						<div class="control has-icons-left">
+							<!-- {#if $myForm.hasError('password.required')}
+								<input
+									id="c"
+									class="input is-danger"
+									type="password"
+									placeholder="Enter your password"
+									bind:value={$password.value} />
+							{:else} -->
+								<input
+									id="password"
+									class="input"
+									type="password"
+									name="password"
+									placeholder="Enter your password"/>
+							<!-- {/if} -->
+							<span class="icon is-small is-left">
+								<i class="fas fa-key" />
+							</span>
+						</div>
+						<!-- {#if $myForm.hasError('password.required')}
+							<p class="help is-danger">Password is required</p>
+						{/if} -->
+					</div>
+
+					<div class="field">
+						<div class="control">
+							<button class="button is-warning is-fullwidth" type="submit">Log in</button>
+						</div>
+					</div>
+					<a href="/register" use:link>Don't have an account? Sign up now!</a>
+					<div>
+						<div class="divider">OR</div>
+					</div>
+					<SocialMediaButtons/>
+				</form>
+			</div>
+		</div>
+	</div>
 </HundredVh>
