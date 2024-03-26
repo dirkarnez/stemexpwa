59aa284be55a029ca20f03e20371d4b824568228
dirkarnez <smalldirkalex@gmail.com>
2024-03-26 20:43:52 +0800

    - upload files

--- src/pages/StudentResources/LearningPortfolio/index.svelte
+++ src/pages/StudentResources/LearningPortfolio/index.svelte
@@ -137,11 +137,11 @@
 {#if !!svg}
 	{@html svg.node().outerHTML}
 {/if}
-	<br />
-	<button class="button is-link" on:click={a}>
-		<span class="icon is-small">
-			<i class="fa fa-download"></i>
-		</span>
-		&nbsp;&nbsp; Download portfolio as PDF
-	</button>
+<br />
+<button class="button is-link" on:click={a}>
+	<span class="icon is-small">
+		<i class="fa fa-download"></i>
+	</span>
+	&nbsp;&nbsp; Download portfolio as PDF
+</button>
 
