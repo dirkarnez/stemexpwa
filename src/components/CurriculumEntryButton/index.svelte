376d91982e142ae65a1040641ef631f31e262fa9
Dirk Arnez <smalldirkalex@gmail.com>
2024-06-05 05:25:22 +0800

    - update index.svelte

--- src/components/CurriculumEntryButton/index.svelte
+++ src/components/CurriculumEntryButton/index.svelte
@@ -8,7 +8,7 @@
 <div class="box m-0 p-0" style={`background-color: ${color}; height: 96px; width: 100%;`}>
     <div class="columns is-mobile m-0 p-0" style={`height: 100%`}>
         <div class="column is-narrow m-0 p-0" style={`height: 100%`}>
-            <img src={getResourcesAPIByID(iconId)} style={`height: 100%; max-width: 96px; border-top-left-radius: 6px; border-bottom-left-radius: 6px; object-fit: cover; `} alt="">
+            <img src={getResourcesAPIByID(iconId)} style={`height: 100%; max-width: 96px; border-top-left-radius: var(--bulma-box-radius); border-bottom-left-radius: var(--bulma-box-radius); object-fit: cover; `} alt="">
         </div>
         <div class={`column is-flex is-align-items-center is-justify-content-center has-text-white`}>
             <p>{description}</p>
